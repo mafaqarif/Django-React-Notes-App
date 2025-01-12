b6f3fe3fd68936177fbab90f878ac16d8ca6d3bd
AfaqArif12345 <muhammad.afaqarif97@gmail.com>
2025-01-12 17:37:40 -0500

    Notes updated- CRUD complete

--- Django/notes_app/notesapp/urls.py
+++ Django/notes_app/notesapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("notes/" , views.notes , name="notes")
+    path("notes/" , views.notes , name="notes"),
+    path("notes/<slug:slugGiven>/", views.notes_detail , name="note-details")
 ]
--- Django/notes_app/notesapp/views.py
+++ Django/notes_app/notesapp/views.py
@@ -19,3 +19,22 @@ def notes(request):
             serializer.save()
             return Response(serializer.data , status=status.HTTP_201_CREATED)
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT","DELETE"])
+def notes_detail(request,slugGiven):
+    try:
+        note = Note.objects.get(slug=slugGiven)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = NoteSerializer(note)
+        return Response(serializer.data , status= status.HTTP_200_OK)
+    elif request.method =="PUT":
+        serializer = NoteSerializer(note , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        return Response(serializer.errors , status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
