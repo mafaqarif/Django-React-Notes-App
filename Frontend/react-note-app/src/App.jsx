49a0cd7039dd175b8819ff806751b35c7c03088e
AfaqArif12345 <muhammad.afaqarif97@gmail.com>
2025-01-23 00:24:25 -0600

    added notes detail & add notes page

--- Frontend/react-note-app/src/App.jsx
+++ Frontend/react-note-app/src/App.jsx
@@ -2,8 +2,8 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage"
-// import AddNotes from "./pages/AddNotes"
-// import NotePage from "./pages/NotePage"
+import AddNotes from "./pages/AddNotes"
+import NotePage from "./pages/NotePage"
 
 const App = () => {
 
@@ -11,8 +11,8 @@ const App = () => {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<MainLayout />} >
       <Route index element={<HomePage />} />
-      {/* <Route path="/add-notes" element={<AddNotes />} />
-      <Route path="/notes-detail" element={<NotePage />} /> */}
+      <Route path="/add-notes" element={<AddNotes />} />
+      <Route path="/notes-detail" element={<NotePage />} />
     </Route>
   ))
 
